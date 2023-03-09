52ce1a976a1241163793ee5d7b550ef3507acc1d
Pius Prince <45078705+piusprince@users.noreply.github.com>
2023-03-09 16:25:55 +0000

    chore: Delete console logs

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,4 @@
 export default function Layout({ children, locale, locales, defaultLocale }) {
-  console.log(`locale is ${locale}`);
-  console.log(`locales are ${locales}`);
-  console.log(`defaultLocale is ${defaultLocale}`);
-
   return (
     <div className="">
       {children}
