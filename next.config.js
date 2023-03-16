b607f7b555b094ffac0f907ac3734e817b72147c
Pius Prince <45078705+piusprince@users.noreply.github.com>
2023-03-16 07:57:00 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,14 +2,14 @@ module.exports = {
   reactStrictMode: true,
   images: {},
   i18n: {
-    locales: ["en", "de", "fr", "en-gb"],
+    locales: ["en", "de", "fr", "en-GB"],
     defaultLocale: "en",
     localeDetection: true,
     domains: [
       {
         domain: "https://multilang-demo-chi.vercel.app",
         defaultLocale: "en",
-        locales: ["en-gb"],
+        locales: ["en-GB"],
       },
       { domain: "https://multilang-fr-eight.vercel.app", defaultLocale: "fr" },
       { domain: "https://multilang-de-nine.vercel.app", defaultLocale: "de" },
